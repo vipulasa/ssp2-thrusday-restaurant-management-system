4861653e7f3d19a20fb9f227888ed31d51c9660a
VIpula S <v.s.anandapiya@gmail.com>
2024-08-22 09:52:53 +0530

    Add UserController for authentication and user retrieval
    Moved user retrieval to UserController and added authentication via POST /user/auth. Refactored existing route to use the new controller method for consistent handling. This improves the application's modularity and security.

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function show(Request $request)
+    {
+        return $request->user();
+    }
+
+    public function authenticate(Request $request)
+    {
+        return rescue(function() use ($request){
+
+            // get only the email and the password
+            $credentials = $request->only('email', 'password');
+
+            // using the details to authenticate the user
+            if (auth()->attempt($credentials)) {
+
+                $user = auth()->user();
+
+                $token = $user->createToken('authToken')->plainTextToken;
+
+                return response()->json([
+                    'token' => $token,
+                    'user' => $user
+                ]);
+
+            }else{
+
+                // create the user if the user does not exist
+                $user = User::create([
+                    'name' => $request->name,
+                    'email' => $request->email,
+                    'password' => Hash::make($request->password)
+                ]);
+
+                $token = $user->createToken('authToken')->plainTextToken;
+
+                return response()->json([
+                    'token' => $token,
+                    'user' => $user
+                ]);
+            }
+
+            // throw an exception if the user is not authenticated
+            throw new \Exception('Invalid credentials');
+
+        }, function($e){
+            return response()->json([
+                'status' => false,
+                'message' => $e->getMessage(),
+                '_time' => now()
+            ], 401);
+        });
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,17 @@
 <?php
 
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+//Route::get('/user', function (Request $request) {
+//    return $request->user();
+//})->middleware('auth:sanctum');
+
+Route::middleware('auth:sanctum')
+    ->get('/user', [UserController::class, 'show']);
+
+Route::post('/user/auth', [UserController::class, 'authenticate']);
+
+
+
